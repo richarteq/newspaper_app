d7bf9e9a8cb3a7717fe2a2b3ffdf2606b1084ea8
Rick <12860829+richarteq@users.noreply.github.com>
2025-06-12 17:13:22 -0500

    Update views.py
    fecha

--- news/views.py
+++ news/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def newsView(request):
-    return HttpResponse('Hello Web from view function. Ate, RickAqp')
+    return HttpResponse('Hello Web from view function. Ate, RickAqp 2025/06/12')
